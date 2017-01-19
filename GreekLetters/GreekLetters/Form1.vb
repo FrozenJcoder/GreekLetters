aa8eca921880586ea12abf6d97f358294c4c53ed
Frozen J-Coder <frozen.john9292@gmail.com>
2017-01-19 21:59:07 +0200

    Add files via upload

---
+++ GreekLetters/GreekLetters/Form1.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Text.RegularExpressions
+
+Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Dim Greek As Regex = New Regex("[^\p{IsGreek}]")
+        TextBox1.Text = Greek.Replace(TextBox1.Text.ToUpper, "")
+
+
+    End Sub
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        'If Not Char.IsLetter(e.KeyChar) Then
+        '    e.Handled = True
+        'End If
+
+
+    End Sub
+
+
+End Class
